5e8be652fa6efb9f9cf5f218e7445619487ce34e
zspro <136277889@qq.com>
2022-02-23 08:52:09 +0800

    Update jd_zjd.js

--- jd_zjd.js
+++ jd_zjd.js
@@ -51,8 +51,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     return;
   }
 
-  // for (let i = 0; i < cookiesArr.length; i++) {
-    for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
       $.UserName = decodeURIComponent(cookie.match(/pt_pin=([^; ]+)(?=;?)/) && cookie.match(/pt_pin=([^; ]+)(?=;?)/)[1])
